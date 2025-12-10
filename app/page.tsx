eacf8a5baf80f7914865ea3784f56031672b1745
Vicente Romero <vromeroan@hotmail.com>
2025-12-10 16:07:10 -0600

    added require login

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
+import RequireLogin from "@/components/RequireLogin";
 import { supabase } from "@/lib/supabase";
 import TaskList from "@/components/TaskList";
 
 export default async function Home() {
-  // Esto ocurre en servidor
-  const userEmail = "... leer desde cookie, no desde localstorage ...";
 
-  const { data: tasks } = await supabase
-    .from("tasks")
-    .select("*")
-    .eq("user_email", userEmail)
-    .order("inserted_at", { ascending: true });
-
-  return <TaskList initialTasks={tasks || []} />;
+  const initialTasks: any[] = [];
+  return (
+    <RequireLogin>
+      <TaskList initialTasks={initialTasks} />
+    </RequireLogin>
+  );
 }
